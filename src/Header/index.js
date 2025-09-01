9313b0606c8d93a7f890c5121321491d717595ee
AlonsoTheDopest <138828524+AlonsoTheDopest@users.noreply.github.com>
2025-09-01 16:55:28 -0700

    Fixed header

--- src/Header/index.js
+++ src/Header/index.js
@@ -3,7 +3,9 @@ import "./index.css"
 export default function Header() {
     return (
         <header>
-            UQLID Lab Topology Optimization Software
+            <h1>
+                UQLID Lab Topology Optimization Software
+            </h1>
         </header>
     )
 }
