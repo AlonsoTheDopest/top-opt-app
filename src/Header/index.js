7b8dfa14ea5f1472c0c6a713c95b88d59ce8dca9
Luke Shahan <lds295@nau.edu>
2025-09-05 14:37:43 -0700

    Update index.js

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,11 +1,16 @@
 import "./index.css"
 
+
 export default function Header() {
     return (
+       
         <header>
-            <h1>
-                UQLID Lab Topology Optimization Software
-            </h1>
+             
+           
+        <h1 class="headerH1">
+            UQLID Lab Topology Optimization Software
+        </h1>
+            
         </header>
     )
 }
