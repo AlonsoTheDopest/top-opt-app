5720d400f247bd1fa67691cfba37f642b59158f9
AlonsoTheDopest <138828524+AlonsoTheDopest@users.noreply.github.com>
2026-02-15 23:05:41 -0700

    added router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,26 +2,24 @@ import './App.css'
 import Header from "./components/Header/Header"
 import Controls from "./components/Controls/Controls"
 import Output from "./components/Output/Output"
-
-import React, { useState } from 'react';
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom"
+import { useState, useEffect } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
 
-function App() {
- 
+function MainMenu()
+{
   const [forceLoadValue, setForceLoadValue] = useState(0);
   const [beamType, setBeamType] = useState('cantilever');
 
   // 1. NEW STATE: Holds the result image from the server
   const [simulationImage, setSimulationImage] = useState(null);
-
   return (
     <Container fluid>
       <Header />
       <Row>
         <Col>
-          
           <Output 
             forceLoadValue={forceLoadValue} 
             setForceLoadValue={setForceLoadValue} 
@@ -31,7 +29,6 @@ function App() {
           /> 
         </Col>
         <Col>
-         
           <Controls 
             beamType={beamType} 
             setBeamType={setBeamType} 
@@ -41,6 +38,46 @@ function App() {
         </Col>
       </Row>
     </Container>
+  )
+}
+
+function Results()
+{
+  const [results, setResults] = useState([])
+  const getResults = async () => {
+    const res = await fetch("/results", {method: "GET"})
+    const data = await res.json()
+    setResults(data)
+  }
+
+  useEffect(() => {
+    getResults();
+  }, [])
+
+  return (
+    <>
+      <h1>Optimization Results</h1>
+      {results.map((r) => (
+        <>
+        <p>Beam Type: {r.beam_type}</p>
+        <p>Load: {r.load}</p>
+        <p>Volume Fraction: {r.volume_fraction}</p>
+        <p>Iterations: {r.iterations}</p>
+        <img src={r.image} style={{maxWidth: "400px"}}/>
+        </>
+      ))}
+    </>
+  )
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainMenu/>}/>
+        <Route path="/results" element={<Results/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
