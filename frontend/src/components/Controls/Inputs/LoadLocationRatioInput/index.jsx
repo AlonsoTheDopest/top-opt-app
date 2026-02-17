c2edba4b6779e65880d06a82b76f52743e52208f
AlonsoTheDopest <138828524+AlonsoTheDopest@users.noreply.github.com>
2026-02-16 19:18:05 -0700

    defined min and max load loc rat

--- frontend/src/components/Controls/Inputs/LoadLocationRatioInput/index.jsx
+++ frontend/src/components/Controls/Inputs/LoadLocationRatioInput/index.jsx
@@ -1,16 +1,45 @@
+import {useEffect} from "react"
+
 export default function LoadLocationRatioInput({
     loadLocationRatio,
-    setLoadLocationRatio
+    setLoadLocationRatio,
+    beamType,
+    length,
+    height
 })
 {
+    let minLoadLocation;
+    let maxLoadLocation;
+    const divisions = 10.0
+
+    if (beamType === "cantilever") {
+        minLoadLocation = 0
+        maxLoadLocation = height
+    } else {
+        minLoadLocation = length / divisions
+        maxLoadLocation = length
+    }
+    const loadLocationStep = maxLoadLocation / divisions
+
+    useEffect(() => {
+        if (beamType === "cantilever")
+        {
+            setLoadLocationRatio(height / 2.0);
+        } 
+        else if( beamType === "half-mbb")
+        {
+            setLoadLocationRatio(length / 2.0);
+        }
+    }, [beamType, length, height, setLoadLocationRatio]);
+
     return (
         <>
-            <label htmlFor="load-location" className="form-label"><p className='controlText'><p className='lastRun'>Load Location Ratio:</p> </p><strong>{loadLocationRatio}</strong></label>
+            <label htmlFor="load-location" className="form-label"><p className='controlText lastRun'>Load Location Ratio &isin; [{minLoadLocation}, {maxLoadLocation}]:</p><strong>{loadLocationRatio}</strong></label>
             <input
                 className="form-range"
                 type="range"
                 name="load"
-                min="0.1" max="0.9" step="0.1"
+                min={minLoadLocation} max={maxLoadLocation} step={loadLocationStep}
                 value={loadLocationRatio}
                 onChange={(e)=>setLoadLocationRatio(e.target.value)}
             />
