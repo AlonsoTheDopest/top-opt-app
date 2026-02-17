b8b49fb09a5b42693d397686ad81a217b662a002
AlonsoTheDopest <138828524+AlonsoTheDopest@users.noreply.github.com>
2026-02-16 18:44:24 -0700

    defined min and max load

--- frontend/src/components/Controls/Inputs/LoadInput/index.jsx
+++ frontend/src/components/Controls/Inputs/LoadInput/index.jsx
@@ -1,5 +1,7 @@
 export default function LoadInput({ load, setLoad })
 {
+    const minLoad = -1
+    const maxLoad = 1
     const handleLoadChange = (e) => {
         let val = parseFloat(e.target.value);
         if (val === 0) val = 0.1; 
@@ -8,12 +10,12 @@ export default function LoadInput({ load, setLoad })
 
     return (
         <>
-            <label htmlFor="load" className="form-label"><p className='controlText'><p className='lastRun'>Load(-1 to 1 with Iterations of 0.1):</p> </p><strong>{load}</strong></label>
+            <label htmlFor="load" className="form-label"><p className='controlText lastRun'>Load &isin; [{minLoad}, {maxLoad}]:</p><strong>{load}</strong></label>
             <input
                 className="form-range"
                 type="range"
                 name="load"
-                min="-1" max="1" step="0.1"
+                min={minLoad} max={maxLoad} step="0.1"
                 value={load}
                 onChange={handleLoadChange}
             />
