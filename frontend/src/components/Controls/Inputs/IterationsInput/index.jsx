0abc5722501a189de4e74f55dfda07611eadf26b
AlonsoTheDopest <138828524+AlonsoTheDopest@users.noreply.github.com>
2026-02-16 18:48:50 -0700

    defined iterations min and max

--- frontend/src/components/Controls/Inputs/IterationsInput/index.jsx
+++ frontend/src/components/Controls/Inputs/IterationsInput/index.jsx
@@ -1,14 +1,17 @@
 export default function IterationsInput({ iterations, setIterations })
 {
+    const iterationsStep = 50
+    const minIterations = iterationsStep
+    const maxIterations = 500
     return (
         <>
-            <label htmlFor="iterations" className="form-label large-control-label">Iterations:</label>
+            <label htmlFor="iterations" className="form-label large-control-label">Iterations &isin; [{minIterations}, {maxIterations}]:</label>
             <input
                 className="form-control large-control-input"
                 type="number"
                 name="iterations"
-                min="0" max="2000"
-                step="100"
+                min={minIterations} max={maxIterations}
+                step={iterationsStep}
                 value={iterations}
                 onChange={(e) => setIterations( Number( e.target.value ) )}
             />
