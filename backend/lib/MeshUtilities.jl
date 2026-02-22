9e8ed5d0a11ec8b593a6168b1937da07d28dcd17
AlonsoTheDopest <138828524+AlonsoTheDopest@users.noreply.github.com>
2026-02-22 16:26:06 -0700

    fixed half-mbb code

--- backend/lib/MeshUtilities.jl
+++ backend/lib/MeshUtilities.jl
@@ -2,88 +2,102 @@
 
 using Gmsh: gmsh, gmsh.model.geo.addPoint, gmsh.model.geo.addLine
 
+const ZERO_DIM = 0;
+const ONE_DIM = 1;
+const TWO_DIM = 2;
+
 function calculateMeshSize(length::Float64)
     return length / 160.0
 end
 
-function createBeamMesh( length::Float64, height::Float64, beam_type::String, load_position::Float64 )
+function createBeamMesh(
+    length::Float64, 
+    height::Float64, 
+    beam_type::String, 
+    load_position::Float64
+)::String
+    mesh_file_name = ""
     gmsh.initialize()
     if gmsh.isInitialized() == 1
-        gmsh.option.setNumber("Mesh.Algorithm", 6) # Frontal-Delaunay for 2D
+        gmsh.option.setNumber("Mesh.Algorithm", 6)
         createBeam(length, height, beam_type, load_position)
-        gmsh.model.mesh.generate(2)
+        gmsh.model.mesh.generate(TWO_DIM)
         gmsh.write("./mesh.msh")
         gmsh.finalize()
-        return "./mesh.msh"
+        mesh_file_name = "./mesh.msh"
     end
-    return ""
+    return mesh_file_name
 end
 
 
-function createBeam( length::Float64, height::Float64, beam_type::String, load_position::Float64 )
-    ptsVec = addPoints( length, height, beam_type, load_position )
-
-    linesVec = addLines( ptsVec )
-
-    area = addArea( linesVec )
-
-    addPhysicalGroups( linesVec, area, beam_type )
+function createBeam(
+    length::Float64,
+    height::Float64,
+    beam_type::String,
+    load_position::Float64
+)
+    ptsVec = addPoints(length, height, beam_type, load_position)
+    linesVec = addLines(ptsVec)
+    area = addArea(linesVec)
+    addPhysicalGroups(ptsVec, linesVec, area, beam_type)
 end
 
 
-function addPhysicalGroups( linesVec, area, beam_type::String )
+function addPhysicalGroups(ptsVec, linesVec, area, beam_type::String)
     gmsh.model.geo.synchronize()
 
-    domainPhysGroup = gmsh.model.addPhysicalGroup( 2, [ area ] )
-    gmsh.model.setPhysicalName( 2, domainPhysGroup, "Domain" )
+    domainPhysGroup = gmsh.model.addPhysicalGroup(TWO_DIM, [area])
+    gmsh.model.setPhysicalName(TWO_DIM, domainPhysGroup, "Domain")
 
     if beam_type == "cantilever" || beam_type == "general"
-        loadLine = linesVec[ 3 ]
-        leftLine = linesVec[ end ]
+        loadLine = linesVec[3]
+        leftLine = linesVec[end]
 
-        loadLinePhysGroup = gmsh.model.addPhysicalGroup( 1, [ loadLine ] )
-        gmsh.model.setPhysicalName( 1, loadLinePhysGroup, "LoadLine" )
+        loadLinePhysGroup = gmsh.model.addPhysicalGroup(ONE_DIM, [loadLine])
+        gmsh.model.setPhysicalName(ONE_DIM, loadLinePhysGroup, "LoadLine")
 
-        leftSupportPhysGroup = gmsh.model.addPhysicalGroup( 1, [ leftLine ] )
-        gmsh.model.setPhysicalName( 1, leftSupportPhysGroup, "LeftSupport" )
+        leftSupportPhysGroup = gmsh.model.addPhysicalGroup(ONE_DIM, [leftLine])
+        gmsh.model.setPhysicalName(ONE_DIM, leftSupportPhysGroup, "LeftSupport")
 
     else # Half-MBB
+        bottom_right_pt = ptsVec[2]
+        loadLine = linesVec[4]
+        leftLine = linesVec[end]
 
-        rightline = linesVec[ 2 ]
-        loadLine = linesVec[ 4 ]
-        leftLine = linesVec[ end ]
+        rightSupportPhysGroup = gmsh.model.addPhysicalGroup(ZERO_DIM, [bottom_right_pt])
+        gmsh.model.setPhysicalName(ZERO_DIM, rightSupportPhysGroup, "RightSupport")
 
-        rightSupportPhysGroup = gmsh.model.addPhysicalGroup( 1, [ rightline ] )
-        gmsh.model.setPhysicalName( 1, rightSupportPhysGroup, "RightSupport")
+        loadLinePhysGroup = gmsh.model.addPhysicalGroup(ONE_DIM, [loadLine])
+        gmsh.model.setPhysicalName(ONE_DIM, loadLinePhysGroup, "LoadLine")
 
-        loadLinePhysGroup = gmsh.model.addPhysicalGroup( 1, [ loadLine ] )
-        gmsh.model.setPhysicalName( 1, loadLinePhysGroup, "LoadLine" )
-
-        leftSupportPhysGroup = gmsh.model.addPhysicalGroup( 1, [ leftLine ] )
-        gmsh.model.setPhysicalName( 1, leftSupportPhysGroup, "LeftSupport" )
+        leftSupportPhysGroup = gmsh.model.addPhysicalGroup(ONE_DIM, [leftLine])
+        gmsh.model.setPhysicalName(ONE_DIM, leftSupportPhysGroup, "LeftSupport")
     end
 end
 
-function addArea( linesVec )
-
-    curveLoop = gmsh.model.geo.addCurveLoop( linesVec )
-    area = gmsh.model.geo.addPlaneSurface( [ curveLoop ] )
-
+function addArea(linesVec)
+    curveLoop = gmsh.model.geo.addCurveLoop(linesVec)
+    area = gmsh.model.geo.addPlaneSurface([curveLoop])
     return area
 end
 
 
-function addPoints( length::Float64, height::Float64, beam_type::String, load_position::Float64 )
+function addPoints(
+    length::Float64, 
+    height::Float64, 
+    beam_type::String, 
+    load_position::Float64 
+)
 
-    mesh_size = calculateMeshSize( length )
+    mesh_size = calculateMeshSize(length)
 
     bottomLeftPt, bottomRightPt, topRightPt, topLeftPt = addCornerPoints( length, height, mesh_size )
-    cornerPtsVec = [ bottomLeftPt, bottomRightPt, topRightPt, topLeftPt ]
+    corner_pts = [ bottomLeftPt, bottomRightPt, topRightPt, topLeftPt ]
 
     loadLinePt1, loadLinePt2 = addLoadLinePoints( length, height, beam_type, mesh_size, load_position )
-    loadLinePtsVec = [ loadLinePt1, loadLinePt2 ]
+    load_line_pts = [ loadLinePt1, loadLinePt2 ]
 
-    ptsVec = assemblePtsVec( cornerPtsVec, loadLinePtsVec, beam_type )
+    ptsVec = assemblePtsVec( corner_pts, load_line_pts, beam_type )
     
     return ptsVec
 end
@@ -93,14 +107,14 @@ function assemblePtsVec( cornerPtsVec, loadLinePtsVec, beam_type::String )
     ptsVec = []
 
     if beam_type == "cantilever" || beam_type == "general"
-        append!( ptsVec, cornerPtsVec[ 1 : 2 ] )
-        append!( ptsVec, loadLinePtsVec )
-        append!( ptsVec, cornerPtsVec[ 3 : 4 ] )
+        append!(ptsVec, cornerPtsVec[1:2])
+        append!(ptsVec, loadLinePtsVec)
+        append!(ptsVec, cornerPtsVec[3:4])
 
     else # Half-MBB
-        append!( ptsVec, cornerPtsVec[ 1 : 3 ] )
-        append!( ptsVec, loadLinePtsVec )
-        append!( ptsVec, cornerPtsVec[ 4 ] )
+        append!(ptsVec, cornerPtsVec[1:3])
+        append!(ptsVec, loadLinePtsVec)
+        append!(ptsVec, cornerPtsVec[4])
     end
 
     return ptsVec
@@ -123,32 +137,30 @@ function addLoadLinePoints( length::Float64,
 end
 
 
-function addLines( ptsVec )
+function addLines(ptsVec)
     lines = []
-
-    numPts = length( ptsVec )
+    numPts = length(ptsVec)
 
     for idx in 1 : numPts
         startPtIdx = idx
-        endPtIdx = mod1( idx + 1, numPts )
+        endPtIdx = mod1(idx + 1, numPts)
 
-        startPt = ptsVec[ startPtIdx ]
-        endPt = ptsVec[ endPtIdx ]
+        startPt = ptsVec[startPtIdx]
+        endPt = ptsVec[endPtIdx]
 
-        push!( lines, addLine( startPt, endPt ) )
+        push!(lines, addLine(startPt, endPt))
     end
 
     return lines
 end
 
 
-function addCornerPoints( length::Float64, height::Float64, mesh_size::Float64 )
-    bottomLeftPt = addPoint( 0.0, 0.0, 0.0, mesh_size )
-    bottomRightPt = addPoint( length, 0.0, 0.0, mesh_size )
-    topRightPt = addPoint( length, height, 0.0, mesh_size )
-    topLeftPt = addPoint( 0.0, height, 0.0, mesh_size )
-
-    return bottomLeftPt, bottomRightPt, topRightPt, topLeftPt
+function addCornerPoints(length::Float64, height::Float64, mesh_size::Float64)
+    bottom_left = addPoint(0.0, 0.0, 0.0, mesh_size)
+    bottom_right = addPoint(length, 0.0, 0.0, mesh_size)
+    top_right = addPoint(length, height, 0.0, mesh_size)
+    top_left = addPoint(0.0, height, 0.0, mesh_size)
+    return bottom_left, bottom_right, top_right, top_left
 end
 
 
@@ -173,29 +185,35 @@ function addCantileverPoints( length::Float64, height::Float64, mesh_size, load_
 end 
 
 
-function addHalfMbbPoints( length::Float64, height::Float64, mesh_size, load_position::Float64 )
+function addHalfMbbPoints(
+    length::Float64, 
+    height::Float64, 
+    mesh_size, 
+    load_position::Float64
+)
     offset = 0.1
-    topSideLeftPtLength = load_position - offset
-    topSideRightPtLength = load_position + offset
+    left_load_line_pt_pos = load_position - offset
+    right_load_line_pt_pos = load_position + offset
 
-    if topSideLeftPtLength <= 0.0
-        topSideLeftPtLength = offset
-        topSideRightPtLength = topSideLeftPtLength + 2.0 * offset
+    if left_load_line_pt_pos <= 0.0
+        left_load_line_pt_pos = offset
+        right_load_line_pt_pos = left_load_line_pt_pos + 2.0 * offset
 
-    elseif topSideRightPtLength >= length
-        topSideRightPtLength = length - offset
-        topSideLeftPtLength = topSideLeftPtLength - 2.0 * offset
+    elseif right_load_line_pt_pos >= length
+        right_load_line_pt_pos = length - offset
+        left_load_line_pt_pos = left_load_line_pt_pos - 2.0 * offset
     end
 
-    topSideLeftPt = addPoint( topSideLeftPtLength, height, 0.0, mesh_size )
-    topSideRightPt = addPoint( topSideRightPtLength, height, 0.0, mesh_size )
+    left_load_line_pt = addPoint(left_load_line_pt_pos, height, 0.0, mesh_size)
+    right_load_line_pt = addPoint(right_load_line_pt_pos, height, 0.0, mesh_size)
 
-    return topSideRightPt, topSideLeftPt
+    return right_load_line_pt, left_load_line_pt
 end
 
-function loadBeamMesh( file_name::String, beam_type::String )
-    model = GmshDiscreteModel( file_name )
-    writevtk(model , beam_type )
+
+function loadBeamMesh(file_name::String, beam_type::String)
+    model = GmshDiscreteModel(file_name)
+    writevtk(model, beam_type)
     return model
 end
 
