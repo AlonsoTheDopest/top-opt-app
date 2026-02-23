a71101ffb52b434a815ccfbad3745efa88ecdc7d
AlonsoTheDopest <138828524+AlonsoTheDopest@users.noreply.github.com>
2026-02-22 18:42:28 -0700

    made load_edge apply the respective points

--- backend/lib/MeshUtilities.jl
+++ backend/lib/MeshUtilities.jl
@@ -14,13 +14,14 @@ function createBeamMesh(
     length::Float64, 
     height::Float64, 
     beam_type::String, 
+    load_edge::String,
     load_position::Float64
 )::String
     mesh_file_name = ""
     gmsh.initialize()
     if gmsh.isInitialized() == 1
         gmsh.option.setNumber("Mesh.Algorithm", 6)
-        createBeam(length, height, beam_type, load_position)
+        createBeam(length, height, beam_type, load_edge, load_position)
         gmsh.model.mesh.generate(TWO_DIM)
         gmsh.write("./mesh.msh")
         gmsh.finalize()
@@ -34,9 +35,10 @@ function createBeam(
     length::Float64,
     height::Float64,
     beam_type::String,
+    load_edge::String,
     load_position::Float64
 )
-    ptsVec = addPoints(length, height, beam_type, load_position)
+    ptsVec = addPoints(length, height, load_edge, load_position)
     linesVec = addLines(ptsVec)
     area = addArea(linesVec)
     addPhysicalGroups(ptsVec, linesVec, area, beam_type)
@@ -84,8 +86,8 @@ end
 
 function addPoints(
     length::Float64, 
-    height::Float64, 
-    beam_type::String, 
+    height::Float64,
+    load_edge::String,
     load_position::Float64 
 )
 
@@ -94,46 +96,88 @@ function addPoints(
     bottomLeftPt, bottomRightPt, topRightPt, topLeftPt = addCornerPoints( length, height, mesh_size )
     corner_pts = [ bottomLeftPt, bottomRightPt, topRightPt, topLeftPt ]
 
-    loadLinePt1, loadLinePt2 = addLoadLinePoints( length, height, beam_type, mesh_size, load_position )
-    load_line_pts = [ loadLinePt1, loadLinePt2 ]
+    loadLinePt1, loadLinePt2 = addLoadLinePoints( 
+        length,
+        height,
+        mesh_size, 
+        load_edge, 
+        load_position 
+    );
+    load_line_pts = [ loadLinePt1, loadLinePt2 ];
 
-    ptsVec = assemblePtsVec( corner_pts, load_line_pts, beam_type )
+    ptsVec = assemblePtsVec(corner_pts, load_line_pts, load_edge)
     
     return ptsVec
 end
 
 
-function assemblePtsVec( cornerPtsVec, loadLinePtsVec, beam_type::String )
+function assemblePtsVec(cornerPtsVec, loadLinePtsVec, load_edge)
     ptsVec = []
 
-    if beam_type == "cantilever" || beam_type == "general"
-        append!(ptsVec, cornerPtsVec[1:2])
-        append!(ptsVec, loadLinePtsVec)
-        append!(ptsVec, cornerPtsVec[3:4])
+    if load_edge == "right"
+        append!(ptsVec, cornerPtsVec[1:2]);
+        append!(ptsVec, loadLinePtsVec);
+        append!(ptsVec, cornerPtsVec[3:4]);
 
-    else # Half-MBB
-        append!(ptsVec, cornerPtsVec[1:3])
-        append!(ptsVec, loadLinePtsVec)
-        append!(ptsVec, cornerPtsVec[4])
+    elseif load_edge == "top"
+        append!(ptsVec, cornerPtsVec[1:3]);
+        append!(ptsVec, loadLinePtsVec[2]);
+        append!(ptsVec, loadLinePtsVec[1]);
+        append!(ptsVec, cornerPtsVec[4]);
+
+    elseif load_edge == "left"
+        append!(ptsVec, cornerPtsVec);
+        append!(ptsVec, loadLinePtsVec[2]);
+        append!(ptsVec, loadLinePtsVec[1]);
+
+    else # bottom
+        append!(ptsVec, cornerPtsVec[1]);
+        append!(ptsVec, loadLinePtsVec);
+        append!(ptsVec, cornerPtsVec[2:4]);
     end
 
-    return ptsVec
+    return ptsVec;
 end
 
 
-function addLoadLinePoints( length::Float64, 
-                            height::Float64, 
-                            beam_type::String, 
-                            mesh_size::Float64,
-                            load_position::Float64 )
-    if beam_type == "cantilever" || beam_type == "general"
-        loadLinePt1, loadLinePt2 = addCantileverPoints( length, height, mesh_size, load_position )
-        
-    elseif beam_type == "half-mbb"
-        loadLinePt1, loadLinePt2 = addHalfMbbPoints( length, height, mesh_size, load_position )
+function getLoadLinePointPositions(load_position, edge_length, offset = 0.1)
+    pt1_position = load_position - offset;
+    pt2_position = load_position + offset;
+
+    if pt2_position >= edge_length
+        pt2_position = edge_length - offset
+        pt1_position = pt2_position - 2.0 * offset
+
+    elseif pt1_position <= 0.0
+        pt1_position = offset
+        pt2_position = pt1_position + 2.0 * offset
     end
 
-    return loadLinePt1, loadLinePt2
+    return pt1_position, pt2_position
+end
+
+
+function addLoadLinePoints( 
+    length::Float64, 
+    height::Float64, 
+    mesh_size::Float64,
+    load_edge::String,
+    load_position::Float64 
+)
+    if load_edge == "right" || load_edge == "left"
+        bottom_pt_position, top_pt_position = getLoadLinePointPositions(load_position, height)
+
+        load_line_pt1 = addPoint(length, bottom_pt_position, 0.0, mesh_size)
+        load_line_pt2 = addPoint(length, top_pt_position, 0.0, mesh_size)
+        
+    elseif load_edge == "top" || load_edge == "bottom"
+        left_pt_position, right_pt_position = getLoadLinePointPositions(load_position, length)
+
+        load_line_pt1 = addPoint(left_pt_position, height, 0.0, mesh_size)
+        load_line_pt2 = addPoint(right_pt_position, height, 0.0, mesh_size)
+    end
+
+    return load_line_pt1, load_line_pt2
 end
 
 
@@ -161,53 +205,6 @@ function addCornerPoints(length::Float64, height::Float64, mesh_size::Float64)
     top_right = addPoint(length, height, 0.0, mesh_size)
     top_left = addPoint(0.0, height, 0.0, mesh_size)
     return bottom_left, bottom_right, top_right, top_left
-end
-
-
-function addCantileverPoints( length::Float64, height::Float64, mesh_size, load_position )
-    offset = 0.1
-    rightSideTopPtHeight = load_position + offset
-    rightSideBottomPtHeight = load_position - offset
-
-    if rightSideTopPtHeight >= height
-        rightSideTopPtHeight = height - offset
-        rightSideBottomPtHeight = rightSideTopPtHeight - 2.0 * offset
-
-    elseif rightSideBottomPtHeight <= 0.0
-        rightSideBottomPtHeight = offset
-        rightSideTopPtHeight = rightSideBottomPtHeight + 2.0 * offset
-    end
-
-    rightSideTopPt = addPoint( length, rightSideTopPtHeight, 0.0, mesh_size )
-    rightSideBottomPt = addPoint( length, rightSideBottomPtHeight, 0.0, mesh_size )
-
-    return rightSideBottomPt, rightSideTopPt
-end 
-
-
-function addHalfMbbPoints(
-    length::Float64, 
-    height::Float64, 
-    mesh_size, 
-    load_position::Float64
-)
-    offset = 0.1
-    left_load_line_pt_pos = load_position - offset
-    right_load_line_pt_pos = load_position + offset
-
-    if left_load_line_pt_pos <= 0.0
-        left_load_line_pt_pos = offset
-        right_load_line_pt_pos = left_load_line_pt_pos + 2.0 * offset
-
-    elseif right_load_line_pt_pos >= length
-        right_load_line_pt_pos = length - offset
-        left_load_line_pt_pos = left_load_line_pt_pos - 2.0 * offset
-    end
-
-    left_load_line_pt = addPoint(left_load_line_pt_pos, height, 0.0, mesh_size)
-    right_load_line_pt = addPoint(right_load_line_pt_pos, height, 0.0, mesh_size)
-
-    return right_load_line_pt, left_load_line_pt
 end
 
 
