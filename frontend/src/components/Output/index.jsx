dbb50f07069d4fb945cf40f3874b99eaba69b794
AlonsoTheDopest <138828524+AlonsoTheDopest@users.noreply.github.com>
2026-02-23 16:11:45 -0700

    added new pics for general case

--- frontend/src/components/Output/index.jsx
+++ frontend/src/components/Output/index.jsx
@@ -4,17 +4,61 @@ import NewPlaceholder from "../../images/black.gif";
 
 import CantileverPic from "../../images/cantilever-beam.jpg";
 import HalfMbbPic from "../../images/half-mbb-beam.jpg";
+import BeamTopLoadEdge from "../../images/beam-top-load-edge.png"
+import BeamBottomLoadEdge from "../../images/beam-bottom-load-edge.png"
+import BeamRightLoadEdge from "../../images/beam-right-load-edge.png"
+import BeamLeftLoadEdge from "../../images/beam-left-load-edge.png"
 
-export default function Output({beamType, simulationImage}) {
+export default function Output({beamType, simulationImage, loadEdge}) {
+    let beamPicture;
+    switch (beamType)
+    {
+        case "cantilever":
+            beamPicture = CantileverPic;
+            break;
+
+        case "half-mbb":
+            beamPicture = HalfMbbPic;
+            break;
+
+        case "general":
+            switch (loadEdge)
+            {
+                case "top":
+                    beamPicture = BeamTopLoadEdge;
+                    break;
+
+                case "bottom":
+                    beamPicture = BeamBottomLoadEdge;
+                    break;
+
+                case "right":
+                    beamPicture = BeamRightLoadEdge;
+                    break;
+
+                case "left":
+                    beamPicture = BeamLeftLoadEdge;
+                    break;
+
+                default:
+                    break;
+            }
+            break;
+
+        default:
+            break;
+            
+    }
     return (
         <div className="output-wrapper">
             {/* --- TOP SECTION: SETUP IMAGES ONLY --- */}
             <div className="imageBodyHolder main-beam-display">
-                {beamType === 'cantilever' ? (
+                {/* {beamType === 'cantilever' ? (
                     <Image src={CantileverPic} fluid />
                 ) : (
                     <Image src={HalfMbbPic} fluid />
-                )}
+                )} */}
+                <Image src={beamPicture} fluid/>
             </div>
 
             {/* --- BOTTOM SECTION: RESULT/LOADING + SLIDER --- */}
