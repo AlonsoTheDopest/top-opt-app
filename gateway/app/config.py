68e12187ad611678fcfda2e51d1541e53d4fef86
AlonsoTheDopest <138828524+AlonsoTheDopest@users.noreply.github.com>
2026-04-19 23:33:52 -0700

    changed porting

--- gateway/app/config.py
+++ gateway/app/config.py
@@ -2,5 +2,5 @@
 ROUTES = {
     "backend": ("http://genie-backend:8000"),
     "frontend": ("http://react-frontend:3000"),
-    "mesh-utils": ("http://mesh-utils:8000")
+    "mesh-utils": ("http://mesh-utils:8001")
 }
